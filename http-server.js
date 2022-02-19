c73120d3bd53c69ac86bd90f57ae8076930dd51b
Tahar <tahar>
2022-02-19 13:02:59 +0100

    nothing

--- http-server.js
+++ http-server.js
@@ -3,7 +3,7 @@ http.createServer(function (request, response) {
     // Send the HTTP header 
     // HTTP Status: 200 : OK
     // Content Type: text/plain
-    console.log('Runing on 8081')
+    console.log('Runing on 8ghzejtduzeyudezyjud081')
     response.writeHead(200, {'Content-Type': 'text/plain'});
     
     // Send the response body as "Hello World"
